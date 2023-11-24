2e7bcee3b339862b5eaf3f750853cb7fe66f4f64
aatame3 <aatame3@aatame3.net>
2023-11-24 17:35:53 +0900

    ドメインを変更

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'note.aatame3.net',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'note.aata.me',
   title: 'aatame3のノート(ブログ?)',
   subtitle: '色々書くやつ',
   lang: 'ja-JP',
